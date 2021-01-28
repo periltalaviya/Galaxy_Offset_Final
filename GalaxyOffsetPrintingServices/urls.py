15f0da1cb57e06df714c0e94f7baf57c1bce4af0
Mukesh Chauhan <mrchauhan490@gmail.com>
2021-01-28 08:08:16 +0530

    Galaxy Offset Ninth Commit

--- GalaxyOffsetPrintingServices/urls.py
+++ GalaxyOffsetPrintingServices/urls.py
@@ -24,3 +24,5 @@ urlpatterns = [
     path('admin1/', include('admin1.urls')),
     path('user/', include('user.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
