78803e836e14196521f52c50dc6f173dee2f7c37
Mukesh Chauhan <mrchauhan490@gmail.com>
2021-02-09 10:21:16 +0530

    Galaxy Offset Nineteenth Commit

--- user/urls.py
+++ user/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="user-home")
+    path('', views.index, name="user-home"),
+    path('product', views.product, name="user-product"),
+    path('packages', views.packages, name="user-packages"),
+    path('aboutUs', views.aboutUs, name="user-about-us"),
+    path('gallary', views.gallary, name="user-gallary"),
+    path('feedback', views.feedback, name="user-feedback"),
+    path('login', views.loginPage, name="user-login"),
+    path('register', views.registerPage, name="user-register"),
 ]
--- user/views.py
+++ user/views.py
@@ -5,3 +5,31 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'user/index.html')
+
+
+def product(request):
+    return render(request, 'user/product.html')
+
+
+def packages(request):
+    return render(request, 'user/packages.html')
+
+
+def aboutUs(request):
+    return render(request, 'user/aboutUs.html')
+
+
+def gallary(request):
+    return render(request, 'user/gallary.html')
+
+
+def feedback(request):
+    return render(request, 'user/feedback.html')
+
+
+def loginPage(request):
+    return render(request, 'user/login.html')
+
+
+def registerPage(request):
+    return render(request, 'user/register.html')
